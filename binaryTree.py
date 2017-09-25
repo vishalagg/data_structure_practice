324631ba6ffe69b666228cf49c85e75bab358896
vishalagg <vishalagg1994@gmail.com>
2017-09-25 12:28:47 +0530

    level_order

---
+++ binaryTree.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        return "<Node: {}>".format(self.value)    
+
+class BinaryTree:
+    def __init__(self, values=None):
+        self.root = None
+        if values:
+            self.insert(values)
+    
+    def insert(self, values, index=0):
+        node = None
+        if index < len(values):
+            node = Node(value=values[index])
+            if not self.root:
+                self.root = node
+            node.left = self.insert(values, index=index*2+1)
+            node.right = self.insert(values, index=index*2+2)
+            
+        return node
+    
+level_order = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+tree = BinaryTree(level_order)
+root = tree.root.value
+child = tree.root.left.right.left.value
+print(child)
