34ede07cd5185f0c76a98c19415f34a19a8305b0
vishalagg <vishalagg1994@gmail.com>
2017-11-14 22:09:36 +0530

    Sieve of Eratosthenes

--- primeUptoN.py
+++ primeUptoN.py
@@ -2,15 +2,17 @@
 import time
 
 def primeUptoN(n):
-    #start = time.time()
+    start = time.time()
     l = [True for i in range(n+1)]
     for i in range(2,n+1):
         if l[i]:
             for j in range(2*i,n+1,i):
                 l[j] = False
-    for i in range(2,n+1):
+                
+    print(time.time()-start)
+    '''for i in range(2,n+1):
         if l[i]:
-            print(i)
+            print(i)'''
     
     
 n = int(input("Enter value of n upto which prime no. is to print: "))
